767916270b51e5434e84c7951c3251f5f3602675
redappleapartments <168492824+redappleapartments@users.noreply.github.com>
2024-12-29 19:06:19 -0600

    lets see

--- js/dark.js
+++ js/dark.js
@@ -1,4 +1,8 @@
 document.getElementById('toggle-dark-mode').addEventListener('click', function() {
     document.body.classList.toggle('bg-dark');
     document.body.classList.toggle('text-light');
+    document.querySelectorAll('.card-dark-mode').forEach(function(card) {
+      card.classList.toggle('bg-dark');
+      card.classList.toggle('text-light');
+    });
 });
