bcd2a039c46a2d34475ac7b192bbec479f798942
redappleapartments <168492824+redappleapartments@users.noreply.github.com>
2024-12-29 18:57:03 -0600

    lets see

---
+++ js/dark.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+    document.body.classList.toggle('bg-dark');
+    document.body.classList.toggle('text-light');
+});
